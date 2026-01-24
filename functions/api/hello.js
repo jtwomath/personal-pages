7f64d68d3069cf747575e590644de037ac3ca134
jtwomath <129672193+jtwomath@users.noreply.github.com>
2026-01-24 21:55:50 +0900

    Update hello.js

--- functions/api/hello.js
+++ functions/api/hello.js
@@ -1,1 +1,8 @@
+export async function onRequestGet(context) {
+  const data = {
+    message: "Hello from server (Pages Functions)!",
+    time: new Date().toISOString(),
+  };
 
+  return Response.json(data);
+}
